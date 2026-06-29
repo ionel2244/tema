ac889f4da5b848192784e9a1099960f5206d035f
ionel2244 <ionutnorocea04@gmail.com>
2026-06-29 22:15:40 +0300

    final

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-c=int(input("introdu o varsta"))
-nume=str(input("introdu numele tau"))
-print("Bine ai venit",nume,"ai ",c,"ani")
-#aici vom face noi modificari  
+from datetime import datetime
+
+
+nume = input("Introdu numele: ")
+data_nasterii = input("Introdu data nașterii (zz-ll-aaaa): ")
+
+data_nasterii = datetime.strptime(data_nasterii, "%d-%m-%Y")
+
+
+azi = datetime.today()
+
+varsta = azi.year - data_nasterii.year
+
+
+if (azi.month, azi.day) < (data_nasterii.month, data_nasterii.day):
+    varsta -= 1
+
+print(f"\nSalut, {nume}!")
+print(f"Ai {varsta} ani.")
