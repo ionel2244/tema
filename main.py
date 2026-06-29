041da106e85320adbfc75913845c63762487624b
ionel2244 <ionutnorocea04@gmail.com>
2026-06-29 21:55:56 +0300

    continuam
    ajad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 c=int(input("introdu o varsta"))
 nume=str(input("introdu numele tau"))
 print("Bine ai venit",nume,"ai ",c,"ani")
-#aici vom face noi modificari  
+#aici vom face noi modificari
+#facem pe ramura ceva 
